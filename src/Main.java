0f6bc80b316fb96563119e90377a262d10dcd7a1
Annie Wu <anniewu2303@gmail.com>
2020-05-02 18:15:36 -0700

    Update design

--- src/Main.java
+++ src/Main.java
@@ -20,72 +20,67 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        Image topWithSearchBar = new Image("file:images/top-with-search.png");
-        ImageView viewTopWithSearchBar = new ImageView(topWithSearchBar);
-        HBox topWithSearchBarHBox = new HBox(viewTopWithSearchBar);
-        topWithSearchBarHBox.setAlignment(Pos.CENTER);
+        Image theImage = new Image("file:images/full.jpg");
+        ImageView viewTheImage = new ImageView(theImage);
+        HBox theImageHBox = new HBox(viewTheImage);
+        theImageHBox.setAlignment(Pos.CENTER);
 
-        VBox pictureVBox = new VBox(topWithSearchBarHBox);
+        VBox pictureVBox = new VBox(theImageHBox);
         pictureVBox.setSpacing(70);
-
-
-        ComboBox<String> languageComboBox = new ComboBox<>();
-        // Top 10 languages used on the web 2020
-        languageComboBox.getItems().addAll("ENG", "CHN", "SPN", "ARA", "POR", "IND", "MAL", "JPN", "RUS", "FRA", "DEU");
-        languageComboBox.getSelectionModel().selectFirst();
-        languageComboBox.setPrefWidth(115);
-        languageComboBox.setStyle("-fx-font-size: 15pt");
-
-        HBox langComboHBox = new HBox(languageComboBox);
-        langComboHBox.setAlignment(Pos.TOP_RIGHT);
-        langComboHBox.setPadding(new Insets(10,0,0,510));
-
-        TextField searchBar = new TextField();
-        searchBar.setFocusTraversable(false);
-        searchBar.setPromptText("Search craigslist");
-        searchBar.setStyle("-fx-font-size: 16pt;");
-        searchBar.setPrefWidth(350);
-
-
-        HBox searchHBox = new HBox(searchBar);
-        searchHBox.setPadding(new Insets(10, 0, 0, 315));
-
-        HBox searchAndLangHBox = new HBox(searchHBox, langComboHBox);
 
 
         ComboBox cityComboBox = new ComboBox();
         cityComboBox.getItems().addAll("Acton", "Agoura Hills", "Alhambra", "Altadena", "Arcadia", "Artesia",
                 "Avalon", "Azusa", "Baldwin Park", "Bell Gardens", "Bellflower", "Beverly Hills", "Burbank",
-                "Calabasas", "Canoga Park", "Canyon Country", "Carson", "Castaic", "Cerritos", "Chatsworth",
-                "Claremont", "Compton", "Covina", "Culver City", "Diamond Bar", "Downey", "Duarte", "El Monte",
-                "El Segundo", "Encino", "Gardena", "Glendale", "Glendora", "Granada Hills", "Hacienda Heights",
-                "Harbor City", "Hawaiian Gardens", "Hawthorne", "Hermosa Beach", "Huntington Park", "Inglewood",
-                "La Canada Flintridge", "La Crescenta", "La Mirada", "La Puente", "La Verne", "Lake Hughes",
-                "Lakewood", "Lancaster", "Lawndale", "Littlerock", "Llano", "Lomita", "Long Beach", "Los Angeles",
-                "Lynwood", "Malibu", "Manhattan Beach", "Marina Del Rey", "Maywood", "Mission Hills", "Monrovia",
-                "Montebello", "Monterey Park", "Montrose", "Mount Wilson", "Newhall", "North Hills", "North Hollywood",
-                "Northridge", "Norwalk", "Pacific Palisades", "Pacoima", "Palmdale", "Palos Verdes Peninsula",
-                "Panorama City", "Paramount", "Pasadena", "Pearblossom", "Pico Rivera", "Playa Del Rey", "Playa Vista",
-                "Pomona", "Porter Ranch", "Rancho Palos Verdes", "Redondo Beach", "Reseda", "Rosemead",
-                "Rowland Heights", "San Dimas", "San Fernando", "San Gabriel", "San Marino", "San Pedro",
-                "Santa Clarita", "Santa Fe Springs", "Santa Monica", "Sherman Oaks", "Sierra Madre", "Signal Hill",
-                "South El Monte", "South Gate", "South Pasadena", "Stevenson Ranch", "Studio City", "Sun Valley",
-                "Sunland", "Sylmar", "Tarzana", "Temple City", "Topanga", "Torrance", "Tujunga", "Valencia",
-                "Valley Village", "Valyermo", "Van Nuys", "Venice", "Verdugo City", "Walnut", "West Covina",
-                "West Hills", "West Hollywood", "Whittier", "Wilmington", "Winnetka", "Woodland Hills");
+                "Calabasas", "Canoga Park", "Carson", "Cerritos", "Claremont", "Compton", "Covina",
+                "Culver City", "Diamond Bar", "Downey", "Duarte", "El Monte", "El Segundo", "Encino", "Gardena",
+                "Glendale", "Glendora", "Hacienda Heights", "Harbor City", "Hawthorne", "Hermosa Beach", "Inglewood",
+                "La Mirada", "La Puente", "La Verne", "Lake Hughes", "Lakewood", "Lancaster", "Littlerock", "Lomita",
+                "Long Beach", "Los Angeles", "Malibu", "Marina Del Rey", "Maywood", "Mission Hills", "Monrovia",
+                "Montebello", "Monterey Park", "Newhall", "North Hills", "North Hollywood", "Northridge", "Norwalk",
+                "Palmdale", "Panorama City", "Paramount", "Pasadena", "Pico Rivera", "Playa Del Rey", "Playa Vista",
+                "Pomona", "Porter Ranch", "Redondo Beach", "Reseda", "Rosemead", "San Dimas", "San Gabriel",
+                "San Marino", "San Pedro", "Santa Clarita", "Santa Monica", "Sherman Oaks",
+                "Sierra Madre", "South El Monte", "South Gate", "South Pasadena", "Studio City", "Sun Valley",
+                "Sunland", "Sylmar", "Temple City", "Topanga", "Torrance", "Valencia",
+                "Van Nuys", "Venice", "Walnut", "West Covina", "West Hollywood", "Whittier");
+
         cityComboBox.getSelectionModel().select("Los Angeles");
-        cityComboBox.setPrefWidth(195);
+        cityComboBox.setPrefWidth(240);
+        cityComboBox.setStyle("-fx-font-size: 16pt; -fx-fill: #243E90");
 
         HBox cityComboHBox = new HBox(cityComboBox);
-        cityComboHBox.setPadding(new Insets(10,0,0,40));
-        cityComboHBox.setStyle("-fx-font-size: 11pt");
+        cityComboHBox.setPadding(new Insets(45,0,0,380));
 
-        VBox javafxVBox = new VBox(searchAndLangHBox, cityComboHBox);
 
-        StackPane stack = new StackPane(pictureVBox, javafxVBox);
-        stack.setPadding(new Insets(20, 0, 0, 0));
 
-        Scene scene = new Scene(stack, 1300, 800);
+        TextField searchBar = new TextField();
+        searchBar.setFocusTraversable(false);
+        searchBar.setPromptText("Search craigslist");
+        searchBar.setStyle("-fx-font-size: 16pt");
+        searchBar.setPrefWidth(530);
+
+
+        HBox searchBarHBox = new HBox(searchBar);
+        searchBarHBox.setPadding(new Insets(32, 0, 0, 25));
+
+        ComboBox<String> languageComboBox = new ComboBox<>();
+        // Top 10 languages used on the web 2020
+        languageComboBox.getItems().addAll("ENG", "CHN", "SPN", "ARA", "POR", "IND", "MAL", "JPN", "RUS", "FRA", "DEU");
+        languageComboBox.getSelectionModel().selectFirst();
+        languageComboBox.setPrefWidth(120);
+        languageComboBox.setStyle("-fx-font-size: 16pt");
+
+        HBox langComboHBox = new HBox(languageComboBox);
+        langComboHBox.setAlignment(Pos.TOP_RIGHT);
+        langComboHBox.setPadding(new Insets(33,0,0,500));
+
+
+        HBox topRowHBox = new HBox(cityComboHBox, searchBarHBox, langComboHBox);
+
+        StackPane stack = new StackPane(pictureVBox, topRowHBox);
+
+        Scene scene = new Scene(stack, 1800, 1000);
 
         scene.getStylesheets().add("style.css");
 
